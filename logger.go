9a0aef2345cde9b6cef7167b03dff1b611e45ada
justinG31 <gomezji@bc.edu>
2021-11-22 16:52:15 -0500

    added runLogger
    function to verify block from particular miner and notify all miners of new block

--- logger.go
+++ logger.go
@@ -29,12 +29,22 @@ func loggerVerify(b *Block) {
 	//end routine and start logger notify
 }
 
-func runLogger(minerArray, pushChan) {
+func runLogger(minerArray, pushChanData newmsg, oldblock *Block) {
 // close current channels with miner
 // select case
+block = newmsg.miner.Block 
+newBlock := createBlock(block.nonce, block.hash, block.diff, &newmsg.miner.Block)
+if (loggerVerify(block) == true){
+	for i := 0; i < minerArray.length(); i++{
+		minerArray[i].notifyChan <- true 
+		minerArray[i].pullChan <- newBlock
+	}
+}
+else{
+	newmsg.miner.pullChan <- oldblock
+}
+ break 
+}
 
-loggerVerify()
 
-//initialize channels to send the block to each miner
-Notify chan <- true 
- }
+
